1e9eee52f4734527345312d7065c504050f4582d
Ed Murphy <edward.murphy@capitalone.com>
2016-10-29 17:50:09 -0400

    Ed : patch

--- src/test/java/com/example/activity/DeckardActivityTest.java
+++ src/test/java/com/example/activity/DeckardActivityTest.java
@@ -1,19 +1,40 @@
 package com.example.activity;
 
+import com.example.BuildConfig;
+
+import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.Mockito;
+import org.powermock.api.mockito.PowerMockito;
+import org.powermock.core.classloader.annotations.PowerMockIgnore;
+import org.powermock.core.classloader.annotations.PrepareForTest;
+import org.powermock.modules.junit4.rule.PowerMockRule;
 import org.robolectric.Robolectric;
-import org.robolectric.RobolectricTestRunner;
+import org.robolectric.RobolectricGradleTestRunner;
 import org.robolectric.annotation.Config;
 
 import static org.junit.Assert.assertTrue;
 
-@RunWith(RobolectricTestRunner.class)
-@Config(manifest = "src/main/AndroidManifest.xml", emulateSdk = 18)
+@RunWith(RobolectricGradleTestRunner.class)
+@Config(constants = BuildConfig.class, sdk = 21)
+@PowerMockIgnore({"org.mockito.*", "org.robolectric.*", "android.*"})
+@PrepareForTest(Static.class)
 public class DeckardActivityTest {
+
+    @Rule
+    public PowerMockRule rule = new PowerMockRule();
 
     @Test
     public void testSomething() throws Exception {
         assertTrue(Robolectric.buildActivity(DeckardActivity.class).create().get() != null);
+    }
+
+    @Test
+    public void testStaticMocking() {
+        PowerMockito.mockStatic(Static.class);
+        Mockito.when(Static.staticMethod()).thenReturn("hello mock");
+
+        assertTrue(Static.staticMethod().equals("hello mock"));
     }
 }
